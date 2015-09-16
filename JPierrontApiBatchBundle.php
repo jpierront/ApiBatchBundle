53ac5ede822ddec224f73bc0e2d9e53c28d98f42
Julien PIERRONT <julien@nikao.fr>
2015-09-16 22:15:58 +0200

    Add the bundle main php class

---
+++ JPierrontApiBatchBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace JPierront\ApiBatchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class JPierrontApiBatchBundle
+ */
+class JPierrontApiBatchBundle extends Bundle
+{
+}
